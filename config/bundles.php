da8190744ccc33577a255127c51ab5c41e173e44
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-03 21:44:19 +0100

    Security + Ice Vich Uploader + Validator + Rate Limiter install

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,8 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Ice\VichUploaderBundle\IceVichUploaderBundle::class => ['all' => true],
 ];
