82dbcd699c562cf76d86e83da7db36fcca6a465c
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-09 18:11:18 +0100

    Manage authentication

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator implements AuthenticationEntryPointInterface
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_security_login';
+
+    private UserRepository $userRepository;
+    private RouterInterface $router;
+
+    public function __construct(UserRepository $userRepository, RouterInterface $router)
+    {
+        $this->userRepository = $userRepository;
+        $this->router = $router;
+    }
+
+    public function supports(Request $request): bool
+    {
+        return self::LOGIN_ROUTE === $request->attributes->get('_route')
+            && $request->isMethod('POST')
+            ;
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        //dd($request);
+        $email = $request->request->get('_username');
+        $password = $request->request->get('_password');
+        $csrfToken = $request->request->get('_csrf_token');
+
+        if (!is_string($email) || !is_string($password) || !is_string($csrfToken)) {
+            throw new UserNotFoundException();
+        }
+
+        return new Passport(
+            new UserBadge($email, function ($userIdentifier) {
+                // Todo : Ajouter ici les vérifications complémentaires sur les utilisateurs (actif...)
+                $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+
+                if (!$user) {
+                    throw new UserNotFoundException();
+                }
+
+                return $user;
+            }),
+            new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge(
+                    'authenticate',
+                    $csrfToken
+                ),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($target = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($target);
+        }
+
+        // Todo : Changer la redirection une fois que la page sera développée.
+        return new RedirectResponse($this->router->generate('app_security_login'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        return new RedirectResponse(
+            $this->router->generate('app_security_login')
+        );
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->router->generate('app_security_login');
+    }
+}
