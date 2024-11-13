23271d7b9f394364c6c3e316ca5375bb4eb833fd
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-13 18:18:41 +0100

    test toast

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,10 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $this->addFlash('success', 'Yes !!!');
+        $this->addFlash('success', 'Yes 2 !!!');
+        $this->addFlash('error', 'No !!!');
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
