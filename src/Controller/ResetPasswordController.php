9c714972ed7bbd7c28b0a9097a02927ae92cac66
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-19 20:37:56 +0100

    Manage base email template Manage reset passwword email

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -169,7 +169,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('contact@vdub-dev.com', 'Salt and Pepper'))
             ->to((string) $user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
