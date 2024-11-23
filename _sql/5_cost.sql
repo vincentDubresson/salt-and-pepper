2ef8245456d76b35c48b62e15c740edb8ceb1c90
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-23 21:17:56 +0100

    Manage Costs

---
+++ _sql/5_cost.sql
@@ -0,0 +1,4 @@
+INSERT INTO `cost` (`id`, `label`, `sort`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 'Faible', 1, 'faible', NOW(), NOW()),
+(2, 'Moyen', 2, 'moyen', NOW(), NOW()),
+(3, 'Élevé', 3, 'eleve', NOW(), NOW());
