f54d08fc083ac4c698becea7b9b7f9105caf9d62
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-23 21:17:56 +0100

    Manage Costs

---
+++ src/Entity/Cost.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\SluggableTrait;
+use App\Entity\Trait\TimestampableTrait;
+use App\Repository\CostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Table(name: '`cost`')]
+#[ORM\Entity(repositoryClass: CostRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Cost
+{
+    use TimestampableTrait;
+    use SluggableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    #[Assert\NotBlank(message: 'Le libellé est obligatoire.')]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Le libellé ne peut pas dépasser 255 caractères.',
+    )]
+    #[Assert\NoSuspiciousCharacters]
+    private string $label;
+
+    #[ORM\Column(type: Types::FLOAT, scale: 2)]
+    #[Assert\Positive(message: 'Le tri doit est positif.')]
+    private float $sort;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function __toString(): string
+    {
+        return $this->label;
+    }
+
+    protected function getSlugSource(): string
+    {
+        return $this->label;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getSort(): float
+    {
+        return $this->sort;
+    }
+
+    public function setSort(float $sort): static
+    {
+        $this->sort = $sort;
+
+        return $this;
+    }
+}
