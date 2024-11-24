5849b58dcb9f4165d3bbd45354a11289153c163b
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-24 11:42:29 +0100

    Manage Cooking Types

---
+++ _sql/6_cooking_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO `cooking_type` (`id`, `label`, `sort`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 'Four', 1, 'four', '2023-12-04 18:04:20', '2023-12-04 18:04:20'),
+(2, 'Plaques', 2, 'plaques', '2023-12-04 18:04:20', '2023-12-04 18:04:20'),
+(3, 'Sans cuisson', 3, 'sans-cuisson', '2023-12-04 18:04:20', '2023-12-04 18:04:20'),
+(4, 'Autre', 4, 'autre', '2023-12-04 18:04:20', '2023-12-04 18:04:20');
