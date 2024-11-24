031612f3b15db6a336d9f01bff18f91e40a88d5d
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-24 13:06:09 +0100

    Manage Units

---
+++ src/Admin/UnitAdmin.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Admin;
+
+use App\Entity\Unit;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\FieldDescription\FieldDescriptionInterface;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollectionInterface;
+use Sonata\DoctrineORMAdminBundle\Filter\StringFilter;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+/**
+ * @extends AbstractAdmin<Unit>
+ */
+class UnitAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->with('common.infos_1', ['class' => 'col-md-6', 'label' => 'common.infos_1'])
+            ->add('label', TextType::class, [
+                'label' => 'common.label',
+                'empty_data' => '',
+                'required' => true,
+            ])
+            ->add('abbreviation', TextType::class, [
+                'label' => 'common.abbreviation',
+                'empty_data' => '',
+                'required' => true,
+            ])
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('label', StringFilter::class, [
+                'label' => 'common.label',
+                'show_filter' => true,
+            ])
+            ->add('abbreviation', StringFilter::class, [
+                'label' => 'common.abbreviation',
+                'show_filter' => true,
+            ])
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->add('label', FieldDescriptionInterface::TYPE_STRING, [
+                'label' => 'common.label',
+            ])
+            ->add('abbreviation', FieldDescriptionInterface::TYPE_STRING, [
+                'label' => 'common.abbreviation',
+            ])
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'label' => 'common.actions',
+                'actions' => [
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ])
+        ;
+    }
+
+    protected function configureRoutes(RouteCollectionInterface $collection): void
+    {
+        $collection->clearExcept(['list', 'create', 'edit', 'delete', 'export']);
+    }
+
+    protected function configureExportFields(): array
+    {
+        return [
+            'ID' => 'id',
+            'LIBELLE' => 'label',
+            'ABREVIATION' => 'abbreviation',
+            'CRÉÉ LE' => 'createdAt',
+            'MODIFIÉ LE' => 'updatedAt',
+        ];
+    }
+}
---
+++ src/Entity/Unit.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\TimestampableTrait;
+use App\Repository\UnitRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Table(name: '`unit`')]
+#[ORM\Entity(repositoryClass: UnitRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Unit
+{
+    use TimestampableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    #[Assert\NotBlank(message: 'Le libellé est obligatoire.')]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Le libellé ne peut pas dépasser 255 caractères.',
+    )]
+    #[Assert\NoSuspiciousCharacters]
+    private string $label;
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    #[Assert\NotBlank(message: 'L\'abréviation est obligatoire.')]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'L\'abréviation ne peut pas dépasser 255 caractères.',
+    )]
+    #[Assert\NoSuspiciousCharacters]
+    private string $abbreviation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function __toString(): string
+    {
+        return $this->label;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getAbbreviation(): string
+    {
+        return $this->abbreviation;
+    }
+
+    public function setAbbreviation(string $abbreviation): static
+    {
+        $this->abbreviation = $abbreviation;
+
+        return $this;
+    }
+}
