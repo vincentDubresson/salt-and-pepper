aad3b85746357f5dc8f236aea6b64393814dede5
vincentDubresson <vincent.dubresson@orange.fr>
2024-11-24 13:27:45 +0100

    Add categories sql files

---
+++ _sql/8_category.sql
@@ -0,0 +1,9 @@
+INSERT INTO `category` (`id`, `label`, `slug`, `created_at`, `updated_at`, `sort`) VALUES
+(1, 'Les bases', 'les-bases', NOW(), NOW(), 1),
+(2, 'Apéritif', 'aperitif', NOW(), NOW(), 2),
+(3, 'Entrées', 'entrees', NOW(), NOW(), 3),
+(4, 'Plats', 'plats', NOW(), NOW(), 4),
+(5, 'Desserts', 'desserts', NOW(), NOW(), 5),
+(6, 'Boissons', 'boissons', NOW(), NOW(), 6),
+(7, 'Petits déjeuners et Brunchs', 'petits-dejeuners-et-brunchs', NOW(), NOW(), 7);
+
