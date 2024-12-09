00fe8ae1e34e3daad591a09fe74496a7b5aaa92e
vincentDubresson <vincent.dubresson@orange.fr>
2024-12-09 21:09:41 +0100

    Mangage confidential policy

--- src/Controller/Legal/LegalController.php
+++ src/Controller/Legal/LegalController.php
@@ -3,13 +3,24 @@
 namespace App\Controller\Legal;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LegalController extends AbstractController
 {
     #[Route('/conditions-generales-utilisation', name: 'app_legal_cgu')]
-    public function cgu(): \Symfony\Component\HttpFoundation\Response
+    public function cgu(): Response
     {
         return $this->render('legal/cgu.html.twig');
+    }
+
+    #[Route('/politique-protection-donnees', name: 'app_legal_rgpd')]
+    public function rgpd(): Response
+    {
+        // Todo : Commande de suppression de compte après 12 mois
+        // Todo : EventListener pour log d'ip + user agent des connexions
+        // Todo : EventListener pour log d'ip + user agent des demandes de réinitialisation de mdp
+        // Todo : Permettre la suppression de son compte
+        return $this->render('legal/rgpd.html.twig');
     }
 }
