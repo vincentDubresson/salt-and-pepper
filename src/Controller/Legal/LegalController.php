4b54cf0554499c5b7b52cbcd1a2f99f9cbfede24
vincentDubresson <vincent.dubresson@orange.fr>
2024-12-14 11:53:14 +0100

    Add login log in Google authentication service.

--- src/Controller/Legal/LegalController.php
+++ src/Controller/Legal/LegalController.php
@@ -17,9 +17,6 @@ class LegalController extends AbstractController
     #[Route('/politique-protection-donnees', name: 'app_legal_rgpd')]
     public function rgpd(): Response
     {
-        // Todo : Commande de suppression de compte après 12 mois
-        // Todo : EventListener pour log d'ip + user agent des connexions
-        // Todo : EventListener pour log d'ip + user agent des demandes de réinitialisation de mdp
         // Todo : Permettre la suppression de son compte
         return $this->render('legal/rgpd.html.twig');
     }
